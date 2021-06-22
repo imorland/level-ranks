8645e3831f9cef67db27acfc70b9673583f587c7
Ian Morland <ian@morland.me>
2021-06-22 11:21:12 +0100

    Lint JS

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,13 +1,10 @@
 import app from 'flarum/admin/app';
-//import LevelRanksSettingsModal from './components/LevelRanksSettingsModal';
 
 app.initializers.add('ianm-level-ranks', () => {
-    //app.extensionSettings['reflar-level-ranks'] = () => app.modal.show(new LevelRanksSettingsModal());
-    app.extensionData.for('ianm-level-ranks')
-        .registerSetting({
-            label: app.translator.trans('ianm-level-ranks.admin.settings.levelText'),
-            setting: 'ianm-level-ranks.pointsText',
-            placeholder: app.translator.trans('ianm-level-ranks.lib.defaults.level'),
-            type: 'text',
-        });
+    app.extensionData.for('ianm-level-ranks').registerSetting({
+        label: app.translator.trans('ianm-level-ranks.admin.settings.levelText'),
+        setting: 'ianm-level-ranks.pointsText',
+        placeholder: app.translator.trans('ianm-level-ranks.lib.defaults.level'),
+        type: 'text',
+    });
 });
