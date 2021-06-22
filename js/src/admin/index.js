9145b6124856125320271fa4d6f9e4aae0d399db
Ian Morland <ian@morland.me>
2021-06-22 11:37:29 +0100

    Revive for Flarum 1.0 (#1)
    * First pass over for v1
    
    * Apply fixes from StyleCI
    
    * Use new Tooltip
    
    * Lint JS
    
    * Update setting key
    
    * Newline
    
    Co-authored-by: Ian Morland <imorland@users.noreply.github.com>

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,10 @@
-import app from 'flarum/app';
-import LevelRanksSettingsModal from './components/LevelRanksSettingsModal';
+import app from 'flarum/admin/app';
 
-app.initializers.add('reflar-level-ranks', () => {
-    app.extensionSettings['reflar-level-ranks'] = () => app.modal.show(new LevelRanksSettingsModal());
+app.initializers.add('ianm-level-ranks', () => {
+    app.extensionData.for('ianm-level-ranks').registerSetting({
+        label: app.translator.trans('ianm-level-ranks.admin.settings.levelText'),
+        setting: 'ianm-level-ranks.pointsText',
+        placeholder: app.translator.trans('ianm-level-ranks.lib.defaults.level'),
+        type: 'text',
+    });
 });
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,14 +1,14 @@
-import app from 'flarum/app';
-import { extend } from 'flarum/extend';
-import PostUser from 'flarum/components/PostUser';
+import app from 'flarum/forum/app';
+import { extend } from 'flarum/common/extend';
+import PostUser from 'flarum/forum/components/PostUser';
 import LevelBar from './components/LevelBar';
 
-app.initializers.add('reflar-level-ranks', app => {
-	extend(PostUser.prototype, 'view', function (view) {
-    const user = this.props.post.user();
+app.initializers.add('reflar-level-ranks', (app) => {
+    extend(PostUser.prototype, 'view', function (view) {
+        const user = this.attrs.post.user();
 
-    if (!user) return;
+        if (!user) return;
 
-		view.children.push(LevelBar.component({ user }))
-	});
+        view.children.push(LevelBar.component({ user }));
+    });
 });
